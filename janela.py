e469c565cb1dee7f8f03b4d91f4950b9b44bcf63
UnderClany <77458377+UnderClany@users.noreply.github.com>
2024-07-12 16:41:04 -0300

    version 1.2

--- janela.py
+++ janela.py
@@ -1,13 +1,10 @@
-
 from tkinter import *
 import customtkinter as ctk
 import sqlite3
 from tkinter import messagebox
-from janela_ferramentas import JanelaMenu
+from janela_ferramentas import JanelaMenu  # Certifique-se de que JanelaMenu está no arquivo correto
 
-
-
-class BackEnd():
+class BackEnd:
     def conecta_db(self):
         self.conn = sqlite3.connect('database.db')
         self.cursor = self.conn.cursor()
@@ -19,8 +16,7 @@ class BackEnd():
 
     def cria_tabela(self):
         self.conecta_db()
-
-        self.cursor.execute(''' 
+        self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS Usuarios (
             Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             Username TEXT NOT NULL,
@@ -38,53 +34,55 @@ class BackEnd():
         self.email_cadastro = self.email_cadastro_entry.get()
         self.senha_cadastro = self.senha_cadastro_entry.get()
         self.confirma_senha_cadastro = self.confirma_senha_entry.get()
-
         self.conecta_db()
 
-
-        self.cursor.execute('''
-            INSERT INTO Usuarios (Username, Email, Senha, Confirma_Senha)
-            VALUES (?, ?, ?, ?)''',(self.username_cadastro, self.email_cadastro, self.senha_cadastro, self.confirma_senha_cadastro))
-
         try:
-            if(self.username_cadastro == '' or self.email_cadastro == '' or self.senha_cadastro == '' or self.confirma_senha_cadastro == ''):
-                messagebox.showerror(title='Sistema de login', message= 'ERRO!!\nPor favor preencha todos os campos!')
-            elif(len(self.username_cadastro) < 4 ):
-                messagebox.showerror(title='Sistema de Login', message='O nome de usuário deve ter pelo menos 4 caracteres.')
-            elif(len(self.senha_cadastro or self.confirma_senha_cadastro) < 4 ):
-                messagebox.showerror(title='Sistema de Login', message='A senha deve conter mais que 4 caracteres.')
-
-            elif(self.senha_cadastro != self.confirma_senha_cadastro):
-                messagebox.showerror(title='Sistema de login',message= 'ERRO!\nAs senhas colocadas não são iguais.')
+            if self.username_cadastro == '' or self.email_cadastro == '' or self.senha_cadastro == '' or self.confirma_senha_cadastro == '':
+                messagebox.showerror('Sistema de login', 'ERRO!!\nPor favor preencha todos os campos!')
+            elif len(self.username_cadastro) < 4:
+                messagebox.showerror('Sistema de Login', 'O nome de usuário deve ter pelo menos 4 caracteres.')
+            elif len(self.senha_cadastro or self.confirma_senha_cadastro) < 4:
+                messagebox.showerror('Sistema de Login', 'A senha deve conter mais que 4 caracteres.')
+            elif self.senha_cadastro != self.confirma_senha_cadastro:
+                messagebox.showerror('Sistema de login', 'ERRO!\nAs senhas colocadas não são iguais.')
             else:
+                self.cursor.execute('''
+                INSERT INTO Usuarios (Username, Email, Senha, Confirma_Senha)
+                VALUES (?, ?, ?, ?)''', (self.username_cadastro, self.email_cadastro, self.senha_cadastro, self.confirma_senha_cadastro))
                 self.conn.commit()
-                messagebox.showinfo(title='Cadastro', message=f'Parabens {self.username_cadastro}\n Casdastro realizado com sucesso!')
+                messagebox.showinfo('Cadastro', f'Parabens {self.username_cadastro}\nCadastro realizado com sucesso!')
                 self.desconecta_db()
                 self.tela_login()
         except:
-            messagebox.showerror(title='Cadastro', message='Erro no processamento do seu cadasttro!\n Por favor tente novamente!')
+            messagebox.showerror('Cadastro', 'Erro no processamento do seu cadastro!\nPor favor tente novamente!')
             self.desconecta_db()
 
     def verifica_login(self):
         self.username_login = self.username_login_entry.get()
         self.senha_login = self.senha_login_entry.get()
-
         self.conecta_db()
 
-        self.cursor.execute('''SELECT * FROM Usuarios WHERE (Username = ? AND Senha = ?)''',
-                            (self.username_login, self.senha_login))
+        try:
+            self.cursor.execute('SELECT * FROM Usuarios WHERE Username = ? AND Senha = ?',
+                                (self.username_login, self.senha_login))
+            self.verifica_dados = self.cursor.fetchone()
 
-        self.verifica_dados = self.cursor.fetchone()  # Percorrendo na tabela usuarios
+            if self.verifica_dados:
+                messagebox.showinfo('Login', f'Parabéns {self.username_login}\nLogin feito com sucesso!')
+                self.abrir_nova_tela(self.username_login)
+            else:
+                messagebox.showerror('Login', 'ERRO!!\nDados não encontrados.\nPor favor digite novamente seus dados ou cadastre-se no sistema!')
+                self.limpa_entry_login()
+        except sqlite3.Error as e:
+            messagebox.showerror('Login', f'Erro ao verificar login: {e}')
+        finally:
+            self.desconecta_db()
 
-        if self.verifica_dados:
-            messagebox.showinfo(title='Login', message=f'Parabéns {self.username_login}\nLogin feito com sucesso!')
-            self.desconecta_db()
-            self.abrir_nova_tela()
-        else:
-            messagebox.showerror(title='Login',
-                                 message='ERRO!!\n Dados não encontrados.\n Por favor digite novamente seus dados ou cadastre-se no sistema! ')
-            self.limpa_entry_login()
-            self.desconecta_db()
+    def abrir_nova_tela(self, usuario):
+        self.destroy()  # Fechando janela principal
+        self.janelaferramentas = JanelaMenu(usuario)
+        self.janelaferramentas.mainloop()
+
 class App(ctk.CTk, BackEnd):
     def __init__(self):
         super().__init__()
@@ -92,15 +90,14 @@ class App(ctk.CTk, BackEnd):
         self.tela_login()
         self.cria_tabela()
 
-
-    #Conf Janela Principal
+    # Configuração da Janela Principal
     def conf_janela_ini(self):
         self.geometry('700x400')
         self.title('Tela de Login')
         self.resizable(False, False)
 
     def tela_login(self):
-        #Trabalhando com as imagens
+        # Trabalhando com as imagens
         self.img = PhotoImage(file='Imagens/ppe1.png')
         self.lb_img = ctk.CTkLabel(self, text=None, image=self.img)
         self.lb_img.grid(row=1, column=0, padx=10, pady=20)
@@ -109,93 +106,94 @@ class App(ctk.CTk, BackEnd):
         self.title = ctk.CTkLabel(self, text='Entre em sua conta e tenha \n acesso a plataforma', font=('Roboto', 24, 'bold'), text_color='#EEAD2D')
         self.title.grid(row=0, column=0, padx=20, pady=20)
 
-        #criar o frame do formulario de login
+        # Criar o frame do formulário de login
         self.frame_login = ctk.CTkFrame(self, width=350, height=380)
         self.frame_login.place(x=350, y=50)
 
-        #Colocando widgets dentro do frame - formulário de login
+        # Colocando widgets dentro do frame - formulário de login
         self.lb_title = ctk.CTkLabel(self.frame_login, text='Faça o seu login!', font=('Roboto', 22, 'bold'))
         self.lb_title.grid(row=0, column=0, padx=10, pady=10)
 
-        #lbl usuário
+        # lbl usuário
         self.username_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text='Usuário', font=('Roboto bold', 16, 'bold'), corner_radius=15)
         self.username_login_entry.grid(row=1, column=0, padx=10, pady=10)
 
-        #lbl senha
+        # lbl senha
         self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text='Senha', font=('Roboto bold', 16, 'bold'), corner_radius=15, show='*')
         self.senha_login_entry.grid(row=2, column=0, padx=10, pady=10)
 
-        #check box ver senha
+        # check box ver senha
         self.ver_senha = ctk.CTkCheckBox(self.frame_login, text='Clique para ver a senha', font=('Roboto bold', 16, 'bold'), corner_radius=20)
         self.ver_senha.grid(row=3, column=0, padx=10, pady=10)
 
-        #btn login
+        # btn login
         self.btn_login = ctk.CTkButton(self.frame_login, width=300, text='Login'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20, command=self.verifica_login)
         self.btn_login.grid(row=4, column=0, padx=10, pady=10)
 
-        #btn cadastro
+        # btn cadastro
         self.span = ctk.CTkLabel(self.frame_login, text='Não possuí uma conta?', font=('Roboto', 12, 'bold'))
-        self.span.grid(row=5, column=0, padx=20, pady=10, sticky='W')
-
-        #btn cadastro
-        self.btn_cadastro = ctk.CTkButton(self.frame_login, width=140, fg_color='green', hover_color='#050', text='Cadastre-se', font=('Roboto bold', 16), corner_radius=15, command=self.tela_cadastro)
-        self.btn_cadastro.grid(row=5, column=0, padx=0, pady=15, sticky='E')
+        self.span.grid(row=5, column=0, padx=10, pady=10)
+        self.btn_cadastro = ctk.CTkButton(self.frame_login, width=300, text='Cadastrar-se'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20, fg_color='#EEAD2D', command=self.tela_cadastro)
+        self.btn_cadastro.grid(row=6, column=0, padx=10, pady=10)
 
     def tela_cadastro(self):
-        #Remover o formulário de Login
-        self.frame_login.place_forget()
+        self.frame_login.destroy()
+        self.geometry('700x520')
+        self.title('Tela de Cadastro')
 
-        #criar o frame do formulario de cadastro
-        self.frame_cadastro = ctk.CTkFrame(self, width=350, height=380)
+        # Trabalhando com as imagens
+        self.img = PhotoImage(file='Imagens/ppe1.png')
+        self.lb_img = ctk.CTkLabel(self, text=None, image=self.img)
+        self.lb_img.grid(row=1, column=0, padx=10, pady=20)
+
+        # Titulo da imagem - janela cadastro
+        self.title = ctk.CTkLabel(self, text='Crie uma conta \n e tenha acesso a plataforma', font=('Roboto', 24, 'bold'), text_color='#EEAD2D')
+        self.title.grid(row=0, column=0, padx=10, pady=10)
+
+        # Criar o frame do formulário de cadastro
+        self.frame_cadastro = ctk.CTkFrame(self, width=350, height=580)
         self.frame_cadastro.place(x=350, y=50)
 
-        #Criando widgets do form cadastro
-        #titulo frorm cadastro
-        self.lb_title = ctk.CTkLabel(self.frame_cadastro, text='Cadastre-se', font=('Roboto', 22, 'bold'))
+        # Colocando widgets dentro do frame - formulário de cadastro
+        self.lb_title = ctk.CTkLabel(self.frame_cadastro, text='Cadastrar-se', font=('Roboto', 22, 'bold'))
         self.lb_title.grid(row=0, column=0, padx=10, pady=10)
 
-        #lbl cad_usuário
+        # lbl usuário
         self.username_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text='Usuário', font=('Roboto bold', 16, 'bold'), corner_radius=15)
         self.username_cadastro_entry.grid(row=1, column=0, padx=10, pady=10)
 
-        #lbl cad_email
-        self.email_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text='E-mail', font=('Roboto bold', 16, 'bold'), corner_radius=15)
+        # lbl email
+        self.email_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text='Email', font=('Roboto bold', 16, 'bold'), corner_radius=15)
         self.email_cadastro_entry.grid(row=2, column=0, padx=10, pady=10)
 
-        #lbl cad_senha
-        self.senha_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text='Senha', font=('Roboto bold', 16, 'bold'), corner_radius=15)
+        # lbl senha
+        self.senha_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text='Senha', font=('Roboto bold', 16, 'bold'), corner_radius=15, show='*')
         self.senha_cadastro_entry.grid(row=3, column=0, padx=10, pady=10)
 
-        #lbl conf_senha
-        self.confirma_senha_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text='Confirme sua senha', font=('Roboto bold', 16, 'bold'), corner_radius=15, show='*')
+        # lbl confirmar senha
+        self.confirma_senha_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text='Confirmar Senha', font=('Roboto bold', 16, 'bold'), corner_radius=15, show='*')
         self.confirma_senha_entry.grid(row=4, column=0, padx=10, pady=10)
 
-        #btn cadastrar
-        self.btn_cadastrar = ctk.CTkButton(self.frame_cadastro, width=140, fg_color='#4c6c8c', hover_color='#4682B4', text='Cadastrar', font=('Roboto bold', 16), corner_radius=15, command=self.cadastrar_usuario)
-        self.btn_cadastrar.grid(row=5, column=0, padx=5, pady=15, sticky='E')
+        # btn cadastrar
+        self.btn_cadastro = ctk.CTkButton(self.frame_cadastro, width=300, text='Cadastrar'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20, command=self.cadastrar_usuario)
+        self.btn_cadastro.grid(row=5, column=0, padx=10, pady=10)
 
-        #btn voltar
-        self.btn_voltar = ctk.CTkButton(self.frame_cadastro, width=140, fg_color='#FF0000', hover_color='#8B0000', text='Voltar', font=('Roboto bold', 16), corner_radius=15, command=self.tela_login)
-        self.btn_voltar.grid(row=5, column=0, padx=5, pady=15, sticky='W')
+        # btn voltar
+        self.span = ctk.CTkLabel(self.frame_cadastro, text='Já possui uma conta?', font=('Roboto', 12, 'bold'))
+        self.span.grid(row=6, column=0, padx=10, pady=10)
+        self.btn_voltar = ctk.CTkButton(self.frame_cadastro, width=300, text='Login'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20, fg_color='#EEAD2D', command=self.voltar_login)
+        self.btn_voltar.grid(row=7, column=0, padx=10, pady=10)
 
-    def limpa_entry_cadastro(self):
-        self.username_cadastro_entry.delete(0, END)
-        self.email_cadastro_entry.delete(0, END)
-        self.senha_cadastro_entry.delete(0, END)
-        self.confirma_senha_entry.delete(0, END)
+    def voltar_login(self):
+        self.frame_cadastro.destroy()
+        self.geometry('700x400')
+        self.title('Tela de Login')
+        self.tela_login()
 
     def limpa_entry_login(self):
         self.username_login_entry.delete(0, END)
         self.senha_login_entry.delete(0, END)
 
-    def abrir_nova_tela(self):
-        self.destroy() #fechando janela principal
-        self.janelaferramentas = JanelaMenu()
-        self.janelaferramentas(self)
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = App()
     app.mainloop()
--- janela_ferramentas.py
+++ janela_ferramentas.py
@@ -2,13 +2,14 @@ import customtkinter as ctk
 from tkinter import *
 from PIL import Image, ImageTk
 
-
 class JanelaMenu(ctk.CTk):
-    def __init__(self):
+    def __init__(self, usuario):
         super().__init__()
         self.title("Aplicação")
         self.geometry("1200x600")
+        self.usuario = usuario
         self.criar_menu_superior()
+        self.resizable(False, False)
 
     def item1_command(self):
         print("Item 1 selecionado")
@@ -17,26 +18,49 @@ class JanelaMenu(ctk.CTk):
         print("Item 2 selecionado")
 
     def criar_menu_superior(self):
-        # criar o frame da barra de menu
-        self.frame_login = ctk.CTkFrame(self, width=1200, height=65, fg_color='#4c6c8c')
-        self.frame_login.place(x=0, y=0)
+        # Criar o frame da barra de menu
+        self.frame_menu = ctk.CTkFrame(self, width=1200, height=65, fg_color='#4c6c8c')
+        self.frame_menu.place(x=0, y=0)
 
-
-        #tratando a Imagem Logo
+        # Tratando a imagem do logo
         image_path = 'Imagens/capacete.png'
         img = Image.open(image_path)
-        #redimensionando a imagem
-        img = img.resize((63,63), Image.LANCZOS)
+        # Redimensionando a imagem
+        img = img.resize((63, 63), Image.LANCZOS)
         self.logo = ImageTk.PhotoImage(img)
 
-        #adicionando elementos ao menu
-        self.lb_logo = ctk.CTkLabel(self, text=None, image=self.logo, fg_color='#4c6c8c')
-        self.lb_logo.grid(row=1, column=0, padx=40, pady=1)
+        # Adicionando elementos ao menu
+        self.lb_logo = ctk.CTkLabel(self.frame_menu, text='', image=self.logo, fg_color='#4c6c8c')
+        self.lb_logo.grid(row=0, column=0, padx=40, pady=1)
 
-        #adicionando mensagem de boas vindas
-        self.mensagem = ctk.CTkLabel(self, text=f'Olá{self.username_cadastro}')
+        # Adicionando mensagem de boas-vindas
+        self.mensagem = ctk.CTkLabel(self.frame_menu, text=f'Olá {self.usuario}', font=('Roboto', 16), text_color='White', fg_color='#4c6c8c')
+        self.mensagem.grid(row=0, column=1, padx=900, pady=1)
+
+
+        # configurando label camera
+        self.frame_camera = ctk.CTkFrame(self, width=300, height=400)
+        self.frame_camera.place(x=30, y=130)
+
+        self.titulo = ctk.CTkLabel(self.frame_camera, text='Selecione a Câmera:', font=('Roboto', 22, 'bold'))
+        self.titulo.grid(row=1, column=0, padx=10, pady=(20, 10))
+
+        self.btn_camera1 = ctk.CTkButton(self.frame_camera, width=300, text='Camera 1'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20)
+        self.btn_camera1.grid(row=2, column=0, padx=10, pady=(20, 10))
+
+        self.btn_camera2 = ctk.CTkButton(self.frame_camera, width=300, text='Camera 2'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20)
+        self.btn_camera2.grid(row=3, column=0, padx=10, pady=(20, 10))
+
+        self.btn_camera3 = ctk.CTkButton(self.frame_camera, width=300, text='Camera 3'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20)
+        self.btn_camera3.grid(row=4, column=0, padx=10, pady=(20, 10))
+
+        self.btn_camera4 = ctk.CTkButton(self.frame_camera, width=300, text='Camera 4'.upper(), font=('Roboto bold', 16, 'bold'), corner_radius=20)
+        self.btn_camera4.grid(row=5, column=0, padx=10, pady=(20, 10))
+
+
 
 
 if __name__ == "__main__":
-    app = JanelaMenu()
+    usuario_logado = "Usuário"  # Passe o nome de usuário logado aqui
+    app = JanelaMenu(usuario_logado)
     app.mainloop()
